5cbb54f36899c0e6fd4737a7271adad394802541
BobbyT432 <boleetee@gmail.com>
2020-04-20 14:40:51 -0700

    Added our game loop

--- TempleQuest/TempleQuest/main.cpp
+++ TempleQuest/TempleQuest/main.cpp
@@ -1,1 +1,22 @@
-// Test
+#include <SFML/Graphics.hpp>
+
+// this will not stay in the final release, use this for debugging purposes to mess with the size of the window
+#define WIDTH 1920
+#define HEIGHT 1080
+#define TITLE "Temple Quest"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), TITLE);
+
+	// this is our main game loop, each loop is ONE frame
+	// Possible problem: Depending on how fast the persons computer is, is how fast the game will run (which is bad)
+	// Possible solution: Lock the loop to 60 FPS
+	while (window.isOpen()) 
+	{
+		
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
