2dca725a1c3e8b0812de87d0d611bdb9f6aa8d84
BobbyT432 <boleetee@gmail.com>
2020-04-22 16:01:54 -0700

    Added an enum for which state the game is in, and offered support to create states easily

---
+++ TempleQuest/TempleQuest/state.cpp
@@ -0,0 +1,11 @@
+#include "state.h"
+
+void IntroState::run()
+{
+	std::cout << "INTRO" << std::endl;
+}
+
+void GameState::run()
+{
+	std::cout << "GAME STATE" << std::endl;
+}
--- TempleQuest/TempleQuest/state.h
+++ TempleQuest/TempleQuest/state.h
@@ -1,20 +1,52 @@
 #ifndef STATE
 #define STATE
 
+//----- keep most includes here, if we have too many includes in seperate areas, compile time will be bad -----
+#include "player.h"
+#include "inputhandle.h"
+#include <map>
+
+
 // ok so states will tell us what state the game is in
 // Main Menu is a state, is the game running is a state, pause menu is a state, etc etc
 // I'm only seeing how other people are making games and mostly everyone does this method
+
+// ----- an enum makes it easier to understand what state to switch too, (enums just assign number values to text) -----
+enum State_
+{
+	STATE_INTRO,
+	STATE_GAME,
+	STATE_PAUSE,
+	STATE_CUTSCENE,
+};
+
+// ----- class interface for each state -----
 class State
 {
-private:
-
-public:
-	State();
-	virtual ~State(); // virtual destructor cus we will be inheriting this class
+protected:
 	
-	// State is an abstract class, so therefore we need us some pure virtuals
-	// you can't create a direct object of this class, but you can of its derived classes (as long as you override the functions below)
-	virtual void update() = 0;
-	virtual void render() = 0; 
+public:
+	//State();
+	virtual void run() = 0;
+	virtual ~State() = default; // virtual destructor cus we will be inheriting this class
 };
+
+// ----- STATES -----
+// The intro state will only be played when the game starts
+class IntroState : public State
+{
+public:
+	void run();
+};
+
+// The game state will only be in action when the player should have control (the entire game except cutscenes, etc)
+class GameState : public State
+{
+public:
+	void run();
+};
+
+
+
+
 #endif
