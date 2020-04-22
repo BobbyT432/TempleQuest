911ef1550c952f03c620849ab597cba89bc90c23
BobbyT432 <boleetee@gmail.com>
2020-04-22 16:00:45 -0700

    Organized the comments

--- TempleQuest/TempleQuest/game.h
+++ TempleQuest/TempleQuest/game.h
@@ -6,13 +6,8 @@
 #define HEIGHT 1080
 #define TITLE "Temple Quest"
 
-
-// keep most includes here, if we have too many includes in seperate areas, compile time will be bad 
 // also cool lil trick if you do 'CTRL + .' over a function decleration, it auto makes a definition in its .cpp equivalent
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-#include "state.h"
+#include "statemanager.h"
 
 class Game
 {
@@ -20,23 +15,28 @@ private:
 	sf::RenderWindow *window; // its a pointer so windows can be dynamically created once a new object is made
 	sf::Event event;
 
-	// for delta time
+	// ---- Delta time ----
 	sf::Clock clock;
 	float deltaTime; // delta time is the elapsed time it took for one frame to cycle, this will be useful for that problem where faster computers run the program faster
-	// for FPS
+	
+	// ---- FPS Counter ----
 	sf::Clock fpsClock;
 	float frame;
 	float FPS;
 public:
 	Game();
 	~Game();
-	// init functions
+
+	// ---- Init functions ----
 	void initWindow();
-	// main game loop
+
+	// ---- Main game loop ----
 	void run();
-	// SFML functions
+
+	// ---- SFML functions ----
 	void eventHandler();
-	// Utilities (maybe transfer to a new class idk)
+
+	// ---- Utilities ----
 	void updateDt();
 	void showFPS();
 };
