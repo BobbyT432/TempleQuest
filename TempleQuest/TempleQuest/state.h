8cc0672ac80f349827beb8faced0e99785cd82d5
BobbyT432 <boleetee@gmail.com>
2020-04-22 01:12:37 -0700

    Added a W.I.P. state system

--- TempleQuest/TempleQuest/state.h
+++ TempleQuest/TempleQuest/state.h
@@ -1,12 +1,6 @@
 #ifndef STATE
 #define STATE
 
-// keep most includes here, if we have too many includes in seperate areas, compile time will be bad 
-// also cool lil trick if you do 'CTRL + .' over a function decleration, it auto makes a definition in its .cpp equivalent
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
-
 // ok so states will tell us what state the game is in
 // Main Menu is a state, is the game running is a state, pause menu is a state, etc etc
 // I'm only seeing how other people are making games and mostly everyone does this method
