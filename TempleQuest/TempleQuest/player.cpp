d4e1379d3a359ff490f2e12cac20aab6cb8e8b8d
BobbyT432 <boleetee@gmail.com>
2020-04-23 16:16:20 -0700

    Added animation support and an update() function

--- TempleQuest/TempleQuest/player.cpp
+++ TempleQuest/TempleQuest/player.cpp
@@ -1,12 +1,12 @@
 #include "player.h"
 
-
 Player::Player()
 {
 	// ---- Players attributes ----
 	collider = true;
 	controllable = 1; // this just says that this entity is indeed a player (this is used in the input handler portion of the code)
 	velocity = 2;
+	currentDir = DOWN;
 
 	// ---- Create the player ----
 	entity = new sf::RectangleShape(sf::Vector2f(100, 100));// This is the players figure (its a rectangle to represent the texture which is shaped like so)
@@ -14,13 +14,12 @@ Player::Player()
 	// ---- Assign textures ---- (THIS MAY BE REMOVED)
 	entTexture = new sf::Texture();
 	entTexture->loadFromFile("textures/player.png");
-	
 	entity->setTexture(entTexture);
-	entTexture->setSmooth(false);
-	hold = new Animation(entTexture, sf::Vector2f(7.6, 1), 0.07);
 
-	// ok so we are going to be reading in a sprite sheet, we need to get the SIZE of that sprite sheet and cut it up into proper animations
-	// this helped a lot: https://www.youtube.com/watch?v=-mcnW_6QpYA
+	animate = new Animation(entTexture, sf::Vector2f(7.6, 1), 0.07); // the reason i did (7.6, 1), is because x represents how many sprites, by giving it more leeway it'l skew the image a bit more
+	//entTexture->setSmooth(true);
+
+	
 	
 	// ---- Error check ----
 	if (!entTexture) { std::cout << "**PLAYER.PNG NOT FOUND**" << std::endl; }
@@ -38,12 +37,23 @@ sf::RectangleShape* Player::getEnt()
 	return entity;
 }
 
+// ---- Update will work hand in hand with the main game loop, cleans up the loop a bit and allows us to organize what needs to be updated JUST for the player ----
 void Player::update(float deltaTime)
 {
-	
+	inputHandler.assignCommand(*this, deltaTime); 
 
-	hold->update(0, deltaTime);
-	entity->setTextureRect(hold->uvRect);
+	// ---- If they were walking right, their idle animation needs to be right, etc ----
+	switch (currentDir)
+	{
+	case FORWARD: 
+		break;
+	case RIGHT:
+		break;
+	case DOWN:
+		break;
+	case LEFT:
+		break;
+	}
 }
 
 
--- TempleQuest/TempleQuest/player.h
+++ TempleQuest/TempleQuest/player.h
@@ -1,22 +1,25 @@
 #ifndef PLAYER
 #define PLAYER
 
-#include "entity.h"
-#include "animation.h"
+#include "inputhandle.h"
 
 // ---- The main players class ----
 class Player : public Entity
 {
 private:
 	
+
+	// ---- Unfortunately, we need to declare this in every Entity that needs input handling (like AI), its because InputHandler depends on entity and entity would depend on InputHandler (circular dependency) ----
+	InputHandler inputHandler;
 public:
-	Animation* hold;
+	
+
 	Player();
 	virtual ~Player();
 
 	// --- This is the actual entity, this can be plugged into draw() and it will display the player ----
 	virtual sf::RectangleShape* getEnt();
 
-	void update(float deltaTime); // this will be removed
+	void update(float deltaTime); // CHANGE TO ALL ENTITIES
 };
 #endif
