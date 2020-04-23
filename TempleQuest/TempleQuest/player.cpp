3e9849487983ca9ca8b8a456ead151cd8bcf578f
Bobby Templin <robert.templin@wsu.edu>
2020-04-23 12:17:51 -0700

    Now works in tangent with the animation class

--- TempleQuest/TempleQuest/player.cpp
+++ TempleQuest/TempleQuest/player.cpp
@@ -1,5 +1,6 @@
 #include "player.h"
 
+
 Player::Player()
 {
 	// ---- Players attributes ----
@@ -8,20 +9,18 @@ Player::Player()
 	velocity = 2;
 
 	// ---- Create the player ----
-	entity = new sf::RectangleShape(sf::Vector2f(100, 150));// This is the players figure (its a rectangle to represent the texture which is shaped like so)
+	entity = new sf::RectangleShape(sf::Vector2f(100, 100));// This is the players figure (its a rectangle to represent the texture which is shaped like so)
 
 	// ---- Assign textures ---- (THIS MAY BE REMOVED)
 	entTexture = new sf::Texture();
 	entTexture->loadFromFile("textures/player.png");
-	entTexture->setSmooth(true);
+	
 	entity->setTexture(entTexture);
+	entTexture->setSmooth(false);
+	hold = new Animation(entTexture, sf::Vector2f(7.6, 1), 0.07);
+
 	// ok so we are going to be reading in a sprite sheet, we need to get the SIZE of that sprite sheet and cut it up into proper animations
 	// this helped a lot: https://www.youtube.com/watch?v=-mcnW_6QpYA
-	sf::Vector2u texSize = entTexture->getSize(); // vector2u is vector to an unsigned int (anything positive), we need to get the size of the sprite sheet
-	texSize.x /= 7.6; // how many penguins are on the x? getSize returns the size of the texture in PIXELS, so divide by 6 for 6 sprites 
-	texSize.y /= 5.6;
-	
-	entity->setTextureRect(sf::IntRect(0, 0, texSize.x, texSize.y)); // (left coordinate, right coordinate, width, height)
 	
 	// ---- Error check ----
 	if (!entTexture) { std::cout << "**PLAYER.PNG NOT FOUND**" << std::endl; }
@@ -37,6 +36,14 @@ Player::~Player()
 sf::RectangleShape* Player::getEnt()
 {
 	return entity;
+}
+
+void Player::update(float deltaTime)
+{
+	
+
+	hold->update(0, deltaTime);
+	entity->setTextureRect(hold->uvRect);
 }
 
 
--- TempleQuest/TempleQuest/player.h
+++ TempleQuest/TempleQuest/player.h
@@ -2,6 +2,7 @@
 #define PLAYER
 
 #include "entity.h"
+#include "animation.h"
 
 // ---- The main players class ----
 class Player : public Entity
@@ -9,11 +10,13 @@ class Player : public Entity
 private:
 	
 public:
-	
+	Animation* hold;
 	Player();
 	virtual ~Player();
 
 	// --- This is the actual entity, this can be plugged into draw() and it will display the player ----
 	virtual sf::RectangleShape* getEnt();
+
+	void update(float deltaTime); // this will be removed
 };
 #endif
