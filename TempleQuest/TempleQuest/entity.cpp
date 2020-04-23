a3837a8953e781692320712d81594c1f04ebeb88
Bobby Templin <robert.templin@wsu.edu>
2020-04-22 20:11:06 -0700

    Added a function now draw within the class instead of outside it

--- TempleQuest/TempleQuest/entity.cpp
+++ TempleQuest/TempleQuest/entity.cpp
@@ -3,26 +3,34 @@
 Entity::Entity()
 {
 	sf::Vector2f pos = sf::Vector2f(0, 0);
-	player = 0;
+	controllable = 0;
 	collider = 0;
 
-	playerBod = new sf::RectangleShape(sf::Vector2f(0, 0));// to be removed
-	float velocity = 0;
+	entity = nullptr;
+	entTexture = nullptr;
+
+	velocity = 0;
+	hp = 0;
 }
 
 sf::Vector2f Entity::getPos()
 {
 	// return the objects position
-	return playerBod->getPosition();
+	return entity->getPosition();
 }
 
 bool Entity::isPlayer()
 {
-	if (player == 0) { return false; }
+	if (controllable == 0) { return false; }
 	else { return true; }
 }
 
 float Entity::getVelo()
 {
 	return velocity;
+}
+
+void Entity::draw(sf::RenderWindow& window)
+{
+	window.draw(*entity);
 }
--- TempleQuest/TempleQuest/entity.h
+++ TempleQuest/TempleQuest/entity.h
@@ -5,30 +5,37 @@
 #include <iostream>
 #include <string>
 
+// ---- The base class for which every entity in the game will be derived from ----
 class Entity 
 {
 protected:
+	// ---- Entity attributes ----
 	sf::Vector2f pos; // entity position
 	bool collider; // is this entity collidable?
-	bool player;
-
-	sf::RectangleShape* playerBod; // THIS WILL BE REMOVED
+	bool controllable; // can this entity be controlled by the player?
 	float velocity;
+	float hp;
+
+	// ---- What will be displayed of the entity ----
+	sf::RectangleShape* entity; // This is the ACTUAL entity, the thing that will be displayed to the screen
+	sf::Texture* entTexture; // the entities texture
 public:
 	
 	Entity();
-	//virtual ~Entity();
+	virtual ~Entity() = default;
 
-	// entity info
+	// ---- Entity info ----
 	sf::Vector2f getPos();
 	bool isPlayer(); // a flag that says if this entity is a player controlled entity
+	virtual float getVelo();
 
-	// to be overrided
+	// ---- To be overrided ----
 	//virtual void onInteract(Entity* ent); // this function will perform something when another entity interacts with this objects entity
 
-	// TO BE REMOVED, TESTING PURPOSES
-	virtual sf::RectangleShape* getBod() = 0;
+	// ---- Return the entities figure (what needs to be rendered) ----
+	virtual sf::RectangleShape* getEnt() = 0;
 
-	virtual float getVelo();
+	// ---- SFML Properties ----
+	virtual void draw(sf::RenderWindow& window);
 };
 #endif
